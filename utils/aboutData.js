963d2c36cd8b35e667efc4ae7d754feff27308dd
Chris French <25254995+chris-french@users.noreply.github.com>
2023-05-31 15:25:16 -0500

    updated home page data

--- utils/aboutData.js
+++ utils/aboutData.js
@@ -1,26 +1,27 @@
 const aboutData = {
-  about: [
-    {
-      title: 'Introduction',
-      content:
-        "Welcome! I'm Chris French, a programmer with a diverse portfolio and a unique background in philosophy. My academic training has instilled rigorous problem-solving skills, crucial in navigating complex technological challenges. Having worked at tech giants like Microsoft and Esri, and explored industries as varied as medical, insurance, and GIS, I bring a broad perspective to problem-solving. Currently, I am deeply immersed in the field of data science, fascinated by advanced models such as ChatGPT that can generate nuanced, human-like text.",
-    },
-    {
-      title: 'Full Stack Developer',
-      content:
-        "I've spent several years honing my full-stack development skills, working on both backend and frontend aspects of various projects. I have used an array of modern technologies, languages, and frameworks to build complex and efficient systems. On the frontend, I have used Next.js, Tailwind CSS, and React Flow for web development. On the backend, I have leveraged Node, Python, and SQL for robust and reliable database management. My extensive experience with AWS Cloud Computing, Docker, Linux, RESTful Microservices and APIs further strengthens my ability to build and manage comprehensive web applications. My proficiency in these areas is evident in my projects, including this very webpage you're viewing, and my ability to build REST API-based PaaS, as demonstrated in my work at Esri."
-    },
-    {
-      title: 'Academic Background',
-      content:
-        "My journey to the tech industry began in academia, where I pursued philosophy. During my PhD studies at the University of British Columbia, I delved into the history of analytic philosophy and the philosophy of science, focusing on thinkers like Rudolf Carnap. His work on inductive logic, which has influenced modern Statistical Bayesian Decision Theory, aligns with my interest in the statistical aspects of AI. This unique intersection of philosophy and technology guides my approach to problem-solving in data engineering and AI."
-    },
-    {
-      title: 'Current Projects',
-      content:
-        "I'm always exploring new ways to integrate my academic interests with my tech skills. I'm engrossed in simulation and evolutionary game theory, working with languages like Rust, Nim, and actively learning C++ 17. I find it exciting to experiment with game engines like Unreal Engine 5 and Godot in my spare time. I'm currently working on a small game engine project using C++ for the Vulkan API, which has been an excellent opportunity to apply my C++ 17 knowledge. As I continue to build on my coding repertoire, my current work and projects offer a thrilling blend of philosophy, science, and technology, illustrating the limitless possibilities at this intersection."
-    },
-  ],
-};
-
-module.exports = aboutData;
+    about: [
+      {
+        title: 'Introduction',
+        content:
+          "Hello! I'm Chris French, a seasoned data engineer and full-stack developer with an increased interest in machine learning and AI. I've worked in diverse industries -- medical, insurance, and GIS -- and I've worked at several companies, including Microsoft and Esri. I'm always learning new technologies, and I'm especially intrigued by the capabilities of language learning models like ChatGPT, and eager to leverage such technologies in enhancing automation workflows.",
+      },
+      {
+        title: 'Full Stack Developer',
+        content:
+          "Over the years, I've developed a deep expertise in full-stack development, building robust backend systems and intuitive frontend interfaces. My frontend experience includes extensive use of React-based frameworks such as Gatsby and Next.js, along with the utilization of Tailwind CSS for responsive design. In backend development, I'm proficient in Node.js, Python, and SQL for reliable database management. Leveraging AWS Cloud Computing, Docker, Linux, RESTful Microservices, and APIs, I'm able to design, build, and manage scalable web applications.",
+      },
+      {
+        title: 'Academic Background',
+        content:
+          "While earning my Ph.D. in Philosophy at the University of British Columbia, I specialized in the history and philosophy of science, with a focus on influential thinkers like Rudolf Carnap, whose work on inductive logic has significantly shaped modern Statistical Bayesian Decision Theory. An integral part of my research involved conceptual engineering, an endeavor that aligns with my interest in the design and development of AI and language learning models.",
+      },
+      {
+        title: 'Current Projects',
+        content:
+          "Outside of professional work, I take interest in enhancing my programming capabilities and understanding of various technologies. I'm currently learning advanced aspects of languages like Rust, Nim, and modern C++ (17). In addition, I'm engaged in a project developing a small game engine using C++ for the Vulkan API, and I've also done small projects in Godot and Unreal Engine 5. I love working on simulations, creating fun dynamics from simple game mechanics, and thinking about game design."
+      },
+    ],
+  };
+  
+  module.exports = aboutData;
+  
